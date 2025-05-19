49757fafad112cffc0e9376beecc15d702776edb
thomas <thomas.zeryouh@goons.fr>
2025-05-19 16:08:02 +0200

    ccorrection md = 4

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Container className="main_Container">
         <Row>
-          <Col>
+          <Col md="4">
             <Dish
               name="Tacos à l’unité"
               image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
@@ -22,7 +22,7 @@ function App() {
             />
           </Col>
 
-          <Col>
+          <Col md="4">
             <Dish
               name="Enchiladas"
               image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
@@ -30,7 +30,7 @@ function App() {
             />
           </Col>
 
-          <Col>
+          <Col md="4">
             <Dish
               name="Mole poblano"
               image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
