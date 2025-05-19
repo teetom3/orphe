b096b7394d8e7fc6beffe1ba89a049505ed6e5f3
thomas <thomas.zeryouh@goons.fr>
2025-05-19 13:21:17 +0200

    correction indentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,15 @@
 export default function Header() 
 {
     return(
-<header>
- 
-    <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt="" />
+        <header>
+             <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt="" />
     
-    <nav>
-        <ul>
-            <li><a href="/">Accueil</a></li>
-        </ul>
-    </nav>
-</header>
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                </ul>
+            </nav>
+        </header>
    )
    
 }
