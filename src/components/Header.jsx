a70001ff9d74e463dcdd855752087774499361f3
thomas <thomas.zeryouh@goons.fr>
2025-05-19 15:46:43 +0200

    md = 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,22 +7,24 @@ import "../assets/styles/header.scss";
 
 export default function Header() {
   return (
-    <Navbar>
-      <Container>
-        <NavbarBrand>
-          <img
-            className="NavBrand"
-            src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-            alt=""
-          />
-        </NavbarBrand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/home">Accueil</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar>
+        <Container>
+          <NavbarBrand>
+            <img
+              className="NavBrand"
+              src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+              alt=""
+            />
+          </NavbarBrand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/home">Accueil</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
