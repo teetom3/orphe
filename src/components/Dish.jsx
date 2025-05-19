5865b735e3c6db46910e1f745c56ff71d1937127
thomas <thomas.zeryouh@goons.fr>
2025-05-19 13:57:34 +0200

    correction

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,8 +2,8 @@ export default function Dish({ name, image, price }) {
   return (
     <div>
       <h2>{name}</h2>
-      <img src={image} alt="image d'un plat mexicain" />
-      <p>{price}</p>
+      <img src={image} alt={name} />
+      <p>{price}€</p>
     </div>
   );
 }
