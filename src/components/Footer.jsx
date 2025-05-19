8a2373e1b910876d8c815a6fc9ddf66cab9038f1
thomas <thomas.zeryouh@goons.fr>
2025-05-19 11:53:48 +0200

    correction act 4-5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer () {
 
     return (
-        <>
-        <div>
-            <p>Copyright Mexican Food 2025</p>
-        </div>
+        <footer>
         
-        </>
+            <p>Copyright Mexican Food 2025</p>
+        
+        
+        </footer>
 
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
-import Img_Equestre from '../assets/equestre.jpg';
+
 
 
 export default function Header() 
 {
     return(
-<>
- <div>
-        <img src={Img_Equestre} alt="" />
-    </div>
+<header>
+ 
+    <img src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png' alt="" />
+    
     <nav>
         <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="/">Accueil</a></li>
         </ul>
     </nav>
-</>
+</header>
    )
    
 }
