930343fa916373e78ce1abbe7f9f833ec2e7a281
thomas <thomas.zeryouh@goons.fr>
2025-05-20 10:16:16 +0200

    act9

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
                 name="Tacos à l’unité"
                 image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
                 price={3}
+                isNew={true}
               />
             </Col>
 
@@ -28,6 +29,7 @@ function App() {
                 name="Enchiladas"
                 image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
                 price={12}
+                isNew={false}
               />
             </Col>
 
@@ -36,6 +38,7 @@ function App() {
                 name="Mole poblano"
                 image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
                 price={15}
+                isNew={false}
               />
             </Col>
           </Row>
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,12 +1,19 @@
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 import "../assets/styles/dish.scss";
+import Badge from "react-bootstrap/Badge";
 
-export default function Dish({ name, image, price }) {
+export default function Dish({ name, image, price, isNew }) {
   return (
     <Card>
       <Card.Img variant="top" src={image} fluid />
       <Card.Body>
+        {isNew && (
+          <h4>
+            <Badge bg="primary">Nouveau</Badge>
+          </h4>
+        )}
+
         <Card.Title>{name}</Card.Title>
         <Card.Text>
           <p>{price}€</p>
