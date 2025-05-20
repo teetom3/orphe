079cb27e10585b7febf3e8af82efe3f3c0bc915d
thomas <thomas.zeryouh@goons.fr>
2025-05-20 11:08:43 +0200

    act10

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,45 @@ import Col from "react-bootstrap/Col";
 import "./App.css";
 
 function App() {
+  const dishes = [
+    {
+      name: "Tacos à l’unité",
+      image:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      price: 3,
+      isNew: true,
+    },
+    {
+      name: "Enchiladas",
+      image:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      price: 12,
+      isNew: false,
+    },
+    {
+      name: "Mole poblano",
+      image:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      price: 15,
+      isNew: false,
+    },
+  ];
   return (
     <>
       <Header />
       <main>
         <Container>
           <Row>
-            <Col md="4">
-              <Dish
-                name="Tacos à l’unité"
-                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                price={3}
-                isNew={true}
-              />
-            </Col>
-
-            <Col md="4">
-              <Dish
-                name="Enchiladas"
-                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                price={12}
-                isNew={false}
-              />
-            </Col>
-
-            <Col md="4">
-              <Dish
-                name="Mole poblano"
-                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                price={15}
-                isNew={false}
-              />
-            </Col>
+            {dishes.map((dishe, index) => (
+              <Col md="4" key={index}>
+                <Dish
+                  name={dishe.name}
+                  image={dishe.image}
+                  price={dishe.price}
+                  isNew={dishe.isNew}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
